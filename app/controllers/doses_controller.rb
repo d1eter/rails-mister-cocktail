202bb8cee245569162ba0c06340532865e3f0f1b
Dieter Pankratz <pankratz117@gmail.com>
2019-11-16 17:13:33 +0100

    add feature: as a user I can  create a new doses to a cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,29 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to :root
+    else
+      render 'new'
+    end
+
+  end
+
+  def destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient, :description)
+  end
 end
