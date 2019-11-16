d327162863dcc5b5cbd11741bdba15307f6a5652
Dieter Pankratz <pankratz117@gmail.com>
2019-11-16 16:27:24 +0100

    add feature: as a user I can edit the name of a cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,15 +8,34 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def update
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.update(cocktail_params)
+      redirect_to @cocktail, notice: 'Cocktail was successfully updated.'
+    else
+      render :edit
+    end
   end
 
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # index, show, new, create
   root 'cocktails#index'
-  resources :cocktails, except: :destroy
+  resources :cocktails, except: :destroy do
+    resources :doses, only: %i[create new]
+  end
+  resources :doses, only: [:destroy]
 end
