d39df7ba2aec13084a036d08abe3bf0186fa2fc4
Dieter Pankratz <pankratz117@gmail.com>
2019-11-16 19:32:10 +0100

    double root rotes again

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # index, show, new, create
   root 'cocktails#index'
-  resources :cocktails, except: %[:destroy index] do
+  resources :cocktails, except: %[:destroy] do
     resources :doses, only: %i[create new]
   end
   resources :doses, only: [:destroy]
