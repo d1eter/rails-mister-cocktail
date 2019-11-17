fdf7236e0ede549d9b7a2076948224a8cf6ba917
Dieter Pankratz <pankratz117@gmail.com>
2019-11-17 15:58:50 +0100

    add upload feature to production

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # index, show, new, create
-  resources :cocktails, except: %[:destroy :index] do
+  resources :cocktails, except: %[:destroy <:in></:in>dex] do
     resources :doses, only: %i[create new]
   end
   resources :doses, only: [:destroy]
